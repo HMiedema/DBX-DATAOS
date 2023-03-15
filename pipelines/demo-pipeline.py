393be203144e9159241e07f76bc4bf67db383138
hessel.m.miedema@capgemini.com <hessel.m.miedema@capgemini.com>
2023-03-15 08:51:51 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    Author:    hessel.m.miedema@capgemini.com <hessel.m.miedema@capgemini.com>
    Date:      Wed Mar 15 08:51:51 2023 +0000
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   .gitignore
    	modified:   helpers/data_os.py
    	new file:   pipelines/demo-pipeline.py
    	new file:   typings/__builtins__.pyi

--- pipelines/demo-pipeline.py
+++ pipelines/demo-pipeline.py
@@ -1,7 +1,14 @@
 # Databricks notebook source
 # import helper functions from the current repository
 import helpers.columns_helpers as ch
-import helpers.data_os as dos
+# import helpers.data_os as dos
+from helpers.data_os import Data_OS
+
+# COMMAND ----------
+
+# Create the Data Operating System
+dos = Data_OS(spark)
+# dos.historize_bronze_table_to_silver(bronzeTableName, silverTableName, "full load with key", keys)
 
 # COMMAND ----------
 
@@ -11,10 +18,45 @@ demo_data = [{"name": 'Nelson Mandela', "quote": "The greatest glory in living l
         {"name": 'Bert Lance', "quote": "If it ain’t broke, don’t fix it."}
         ]
 
-bronzeTableName = "amidbronze.raw_test"
-silverTableName = "amidbronze.silver_test"
+bronzeTableName = "amidbronze.bronze_demo"
+silverTableName = "amidbronze.silver_demo"
 keys = ["name"]
+
 
 # COMMAND ----------
 
-dos.historize_bronze_table_to_silver(bronzeTableName, "amidbronze.silver_test_duplicates", "full load without key with possible duplicate rows", keys)
+# Write the demo data to bronze
+# demo_data_dataframe = spark.createDataFrame(demo_data)
+# dos.write_dataframe_to_bronze(demo_data_dataframe, bronzeTableName)
+
+# COMMAND ----------
+
+%sql
+-- Check if the table was created and __ROW_HASH and __LOADED_AT columns were added
+select * from amidbronze.bronze_demo
+
+# COMMAND ----------
+
+# Write the data to silver
+# The data has a clear key: name
+dos.historize_bronze_table_to_silver(bronzeTableName, silverTableName, "full load with key", keys)
+
+# COMMAND ----------
+
+%sql
+-- Check if the table was created and all rows were added
+-- Every silver table will get a __START_AT and __END_AT column to show the validity of the record
+-- The __CHANGE_TYPE shows the action for the record. In this case it will be all insert
+select * from amidbronze.silver_demo
+
+# COMMAND ----------
+
+# Write the same demo data to bronze
+demo_data_dataframe = spark.createDataFrame(demo_data)
+dos.write_dataframe_to_bronze(demo_data_dataframe, bronzeTableName)
+
+# COMMAND ----------
+
+%sql
+-- The exact same records were added to the bronze table with only a different __LOADED_AT value
+select * from amidbronze.bronze_demo
